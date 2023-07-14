94c9ddcdc42bfe1dc4bef681e8fc06f0891b4ebb
Tina Namalima <tinan.tn1@gmail.com>
2023-07-14 10:50:46 +0200

    portifolio done

---
+++ src/components/About.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section id="about">
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                        Hi, I'm Tina
+                        <br className="hidden lg:inline-block" /> Frontend Developer | Structural Engineer
+                    </h1>
+                    <p className="mb-8 leading-relaxed">
+                        I am a passionate front-end developer with a knack for crafting captivating and
+                        user-friendly websites. With expertise in HTML, CSS, JavaScript & React. I specialize
+                        in creating immersive digital experiences that leave a lasting impression.
+                    </p>
+                    <div className="flex justify-center">
+                        <a
+                            href="#contact"
+                            className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 rounded text-lg">
+                            Contact Me
+                        </a>
+                        <a
+                            href="#projects"
+                            className="ml-4 inline-flex text-gray-400 bg-purple-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                            See My Past Work
+                        </a>
+                    </div>
+                </div>
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                    <img
+                        className="object-cover object-center rounded"
+                        alt="hero"
+                        src="./coding3.svg"
+                    />
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default About
---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Cash2Go-Loan-App",
+    subtitle: "React",
+    description:
+      "Collaboration Project",
+    image: "./project-1.gif",
+    link: "https://cash-loan-pq2b6ww1a-joyibeh93.vercel.app/",
+  },
+  {
+    title: "Employee Profile",
+    subtitle: "React",
+    description:
+      "Individual Project",
+    image: "./project-2.gif",
+    link: "https://unique-biscuit-75484c.netlify.app/",
+  },
+  {
+    title: "To-Do App",
+    subtitle: "React",
+    description:
+      "Individual Project",
+    image: "./project-3.gif",
+    link: "https://shiny-jelly-3649ef.netlify.app/",
+  },
+  {
+    title: "Starwars Moviecard",
+    subtitle: "React",
+    description:
+      "Individual Project",
+    image: "./project-4.gif",
+    link: "https://starwars-movie-cards.vercel.app/#",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working  with Tina was an absolute pleasure! Her professionalism, expertise and attention to detail resulted in a visually stunning website that perfectly captured my brand :)",
+    image: "https://randomuser.me/api/portraits/men/20.jpg",
+    name: "Awesome B",
+    company: "Bwilbe.com",
+  },
+  {
+    quote:
+      "I was impressed by her prompt communication, attention to detail and commitment to client satifisfaction. I highly recommend Tina for anyone seeking exceptional web development services",
+    image: "https://randomuser.me/api/portraits/women/18.jpg",
+    name: "Random T",
+    company: "Happy.za",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "HTML",
+  "CSS",
+  "GraphQL",
+  "Material UI",
+];
